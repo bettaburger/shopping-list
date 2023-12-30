433dda982723a8454cc78a169e55858081719de1
stephanie <stephanieha2021@gmail.com>
2023-12-30 13:24:38 -0500

    Initial Shopping-list commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ItemMenu from "./components/ItemMenu";
+import "./styles/App.css";
+
+
+function App() {
+  return <div className="App">
+    <ItemMenu/>
+  </div>
+}
+
+export default App;
---
+++ src/components/ItemMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Item from "./Item";
+import "../styles/Menu.css";
+
+const ItemMenu = () => {
+  return (
+    <div> className="item-menu"
+        <h1 className="title">Add item to list!</
+        h1>
+        <div className="item">
+            <Item/>
+        </div>
+
+    </div>
+  );
+};
+
+export default ItemMenu
